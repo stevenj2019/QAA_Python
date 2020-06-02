55f811f7d41a1f36a388bbec39625cd9b0d6c60b
Steven Johnson <steven.j2019@pm.me>
2020-06-02 09:19:49 +0100

    COTD01/06

--- COTD/COTD01.py
+++ COTD/COTD01.py
@@ -1,8 +1,5 @@
 num_list = list()
-i=2000
-while i in range(2000, 3201):
-    print(i)
+for i in range(2000,3201):
     if i % 7 == 0 and i % 7 ==0:
         num_list.append(i)
-    i=i+1
 print(num_list)
