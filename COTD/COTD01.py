ec03ffb952dfa30001a5bc2959f4e02b07240ea3
Steven Johnson <steven.j2019@pm.me>
2020-06-02 09:16:28 +0100

    cool :)

--- COTD/COTD01.py
+++ COTD/COTD01.py
@@ -1,10 +1,8 @@
 num_list = list()
 i=2000
-while i in range(2000, 3200):
+while i in range(2000, 3201):
     print(i)
-    if i % 7 == 0:
-        if i% 5 != 0:
-            num_list.append(i)
+    if i % 7 == 0 and i % 7 ==0:
+        num_list.append(i)
     i=i+1
-    
 print(num_list)
