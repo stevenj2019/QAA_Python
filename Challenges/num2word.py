1cf9dc757a5b27a05b179861369ab58affcbfd9a
Steven Johnson <steven.j2019@pm.me>
2020-06-02 14:09:21 +0100

    num2word finished

--- Challenges/num2word.py
+++ Challenges/num2word.py
@@ -1,6 +1,52 @@
 import sys
 from collections import deque
 
+def _numSplitter(number):
+    array = list()
+    number = list(number)
+    size = len(number)
+    while size != 0:
+        if size >= 3:
+            temp = [number[len(number)-3], number[len(number)-2], number[len(number)-1]]
+            array.append(temp)
+            del number[len(number)-1]
+            del number[len(number)-1]
+            del number[len(number)-1]
+        elif size >= 2:
+            temp = [number[len(number)-1], number[len(number)-2]]
+            array.append(temp)
+            del number[len(number)-1]
+            del number[len(number)-1]
+        elif size >= 1:
+            array.append(number[0])
+            del number[len(number)-1]
+        else:
+            print("error has occured")
+            break
+        
+        size = len(number)
+
+    return array
+
+def _dequeConv(deque_list):
+    output = list()
+    for i in deque_list:
+        output.append(i)
+    return output
+
+def _numberCompiler(number):
+    tmp_str = list()
+    if number[0] != 0:
+        tmp_str.append(num2words[int(number[0])] + " hundred")
+
+    num = int(number[1] + number[2])
+    if num in num2words.keys():
+        tmp_str.append(num2words[num])
+    else:
+        tmp_str.append(num2words[int(number[1]+"0")])
+        tmp_str.append(num2words[int(number[2])])
+    return tmp_str
+
 num2words = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
              6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
             11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
@@ -13,64 +59,28 @@ units = ["Thousand", "Million", "Billion", "Trillion"]
 
 #example number 4,988,364,712 == [[7, 1, 2], [3, 6, 4], [9, 8, 8], [4]]
 
-try:
-    number = _numSplitter(sys.argv[1])
-    counter == 0
-    output = deque()
-    while counter <= len(number):
-        if counter == 0:
-            counter = counter + 1
-            tmp_str = list()
-            tmp_num = number[0].split("")
-            if tmp_num[0] != 0:
-                tmp_str.append(tmp_num[0] + " hundred")
 
-            num = int(tmp_num[1] + tmp_num[2])
-            if num2words[num].__exists__() == True:
-                tmp_str.append(num2words[num])
-            else:
-                tmp_str.append(num2words[int(tmp_num[1])])
-                tmp_str.append(num2words[int(tmp_num[2])])
-                
-            del number[0]
-            counter = counter + 1
-        else:
-            tmp_num = number[0]
-            tmp_str = []
-            if tmp_num[0] != 0:
-                tmp_str.append(tmp_num[0] + " hundred")
-            
-            num = int(tmp_num[1] + tmp_num[2])
-            if num2words[num].__exists__() == True:
-                tmp_str.append(num2wordsp[num + units[counter]])
-            else:
-                tmp_str.append(num2words[int(tmp_num[1])])
-                tmp_str.append(num2words[int(tmp_num[1])])
-                tmp_str.append(units[counter])
-
-        output.appendleft(tmp_str)
+number = _numSplitter(sys.argv[1]) #3d array
+counter = 0
+output = deque()
+tmp_str = list()
+size = len(number)
+while counter != size:
+    if counter == 0:
+        print(counter)
+        output.append(_numberCompiler(number[0]))
+        del number[0]
+    else:
+        tmp_str = _numberCompiler(number[0])
+        print(tmp_str)
+        tmp_str.append(units[counter])
     
-    print(output.join(" "))
+    print(tmp_str)
+    output.appendleft(tmp_str)
+    counter = counter + 1
 
-except IndexError:
-    print("Parameter required")
-    sys.exit()
-
-def _numSplitter(number):
-    array = list()
-    number = number.split("")
-    size = len(number)
-    while size > 3:
-        if size == 3:
-            temp = [number[len(number)-1], number[len(number)-2], number[len(number)-3]]
-            array.append(temp)
-            del number[:-3]
-        elif size == 2:
-            temp = [number[len(number)-2], number[len(number)-2]]
-            array.append(temp)
-        elif size == 1:
-            array.append(number[0])
-        else:
-            print("error has occured")
-
-    return array
+output = _dequeConv(output)
+for sub in output:
+    for string in sub:
+        print(string, end=' ')
+print("\n")
