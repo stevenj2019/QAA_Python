df6de6e7b2fb017737284d04d50cfd2db451e0cf
Steven Johnson <steven.j2019@pm.me>
2020-06-09 17:33:57 +0100

    comments added

--- code/Alphabetical.py
+++ code/Alphabetical.py
@@ -1,3 +1,8 @@
 def sorter(string):
     return ",".join(sorted(string.split(",")))
 
+    #list = string.split("")
+    #sorted_list = sorted(list)
+    #sorted_str = ",".join(sorted_list)
+    #return sorted_str
+
